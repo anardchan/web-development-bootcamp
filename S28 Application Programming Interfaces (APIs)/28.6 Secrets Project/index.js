f4c0feab44c823cd9a3527f7e11eedd7510f09b9
Bernard Chan <allanbernardchan+github@gmail.com>
2025-08-21 21:25:23 +0800

    Add practice code for Project Files for using APIs in the backend

---
+++ S28 Application Programming Interfaces (APIs)/28.6 Secrets Project/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+app.use(express.static('public'))
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random/");
+    console.log(result.data)
+    res.render("index.ejs", { 
+        secret: result.data.secret,
+        user: result.data.username
+     });
+  } catch (error) {
+    console.log(`[get-secret] error = ${error}`);
+    res.status(500);
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
